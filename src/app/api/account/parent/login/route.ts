2ff6611b781846463a99a6335b13d67e1167fda7
jaykmarBCET <jaykumar3dear@gmail.com>
2025-09-21 11:02:32 +0530

    bug fix

--- src/app/api/account/parent/login/route.ts
+++ src/app/api/account/parent/login/route.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@/generated/prisma";
 import { generateToken } from "@/services/generateToken";
 import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
@@ -29,16 +30,19 @@ export const POST = async (req: NextRequest) => {
       return NextResponse.json({ message: "Something went wrong while generating token" }, { status: 500 });
     }
 
-     
-    
+     const cookieStore = await cookies()
 
-    const res = NextResponse.json({ ...parentAccount }, { status: 200 });
-    return res.cookies.set("parent",token,{
+     cookieStore.set({
+      name:"parent",
+      value:token,
       httpOnly:true,
       sameSite:"strict",
-      secure:process.env.NODE_ENV ==='production'
-    })
+      secure:process.env.NODE_ENV==='production'
+     })
+    
 
+    return NextResponse.json({ ...parentAccount }, { status: 200 });
+    
   } catch (error) {
     const errorInstance = error as Error;
     return NextResponse.json({ message: "Something went wrong", error: errorInstance.message }, { status: 500 });
